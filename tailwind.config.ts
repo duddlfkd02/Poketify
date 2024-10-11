df8f0ab486dfdbe128f9ee0c12400b3409a773ad
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-10-11 15:05:11 +0900

    프로젝트 생성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,15 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        primary: "#0079FF"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
