0994e86f0c84269973b3d6d1c39c55231ab58402
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-10-13 21:56:48 +0900

    add : release_data 타입스크립트 속성 추가

--- src/types/search.ts
+++ src/types/search.ts
@@ -2,6 +2,6 @@ export type SearchTrack = {
   id: string;
   name: string;
   artists: { name: string }[];
-  album: { images: { url: string }[] };
+  album: { images: { url: string }[]; release_date: string };
   popularity: number;
 };
