836c9bc7b956954745d7ec1e34be273456437bfc
강지우 <mac@macui-MacBookPro.local>
2024-10-14 01:01:26 +0900

    fix: layout.tsx -> 루트, 클라이언트 페이지 분리

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -5,7 +5,7 @@ export default function MainPage() {
     <div className="min-h-screen container mx-auto mt-24 ">
       <h1 className=" text-5xl font-bold mb-5">홈</h1>
 
-      <section className="mb-10">
+      <section className="mb-3">
         <h2 className="text-xl font-semibold mb-2">인기 앨범</h2>
       </section>
       <SpotifyPlaylist />
